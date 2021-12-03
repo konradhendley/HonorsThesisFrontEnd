a83024e1c539ec2a9c4f49bf98a729e60b7f0073
unknown <konradh@colostate.edu>
2021-12-02 17:13:57 -0700

    13.2 11/18 axois, signup

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
       <h1>Login</h1>
+      <div v-if="this.$route.query.signupsuccess"  class = "alert alert-success">Thanks for signing up, please log in now.</div>
       </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import axios from "axios";
 
 import myRouter from "./routes.js";
+
+axios.defaults.baseURL = "https://cis410-fa21-hendley-api.azurewebsites.net"
 
 const myApp = createApp(App)
 myApp.use(myRouter);
